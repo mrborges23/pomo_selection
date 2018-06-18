152310e81c517c5c9220dce4d96fc15b1774acc5
mrborges23 <30591620+mrborges23@users.noreply.github.com>
2018-06-18 11:06:06 +0200

    Update pomo_selection.R

--- pomo_selection.R
+++ pomo_selection.R
@@ -18,103 +18,56 @@ a <- rep(1,4)
 b <- rep(0.01,4)
 c <- rep(0.01,6)
 
+#TUNNING MCMC
 tun <- 10^(-1:ceiling(log(data$s,10)))
 sim   <- 10000
 lag   <- 1
 
-#TUNING
-mcmctunninf <- function(tun,sim,lag) {
-  
- mtun <- matrix(NA,ncol=14,nrow=length(tun))
+#function here
+vtun <- mcmctunning(data,a,b,c,tun,sim,lag)
 
-  for (t in 1:length(tun)) {
-  
-    #RANDOM STARTING POINTS
-    pi    <- rgamma(4,1,1)
-    pi    <- pi/sum(pi)
-    rho   <- rexp(6)
-    sigma <- c(1,rexp(3))
-  
-    #MCMC
-    usim <- seq(lag,sim,lag)
-  
-    smcmc <- matrix(NA,ncol=14,nrow=length(usim))
-  
-    p <- 1
-    for (i in 1:sim) {
-    
-      pi    <- mhpi(N,pi,rho,sigma,data,a,tun[t])
-      rho   <- mhrho(N,pi,rho,sigma,data,c,tun[t])
-      sigma <- mhsigma(N,pi,rho,sigma,data,b,tun[t])
-    
-      if (i == usim[p]) {
-        print(paste("% Tunning: ",round(i*100/sim,1)," (",t,"/",length(tun),")",sep=""))
-        smcmc[p,] <- c(pi,rho,sigma)
-        p<-p+1
-      }
-    }
-  
-    for (j in 1:14) {
-      mtun[t,j] <- (length(unique(smcmc[,j]))-1)/nrow(smcmc)
-    }
-  }
-
-  y1 <- mtun[sum(mtun[,1] < 0.234),1]
-  y2 <- mtun[length(tun)-sum(mtun[,1] > 0.234)+1,1]
-  x1 <- tun[sum(mtun[,1] < 0.234)]
-  x2 <- tun[length(tun)-sum(mtun[,1] > 0.234)+1]
-
-  tun1 <- (0.234-y1)*(x2-x1)/(y2-y1)+x1
-
-  y1 <- mtun[sum(mtun[,5] < 0.234),5]
-  y2 <- mtun[length(tun)-sum(mtun[,5] > 0.234)+1,5]
-  x1 <- tun[sum(mtun[,5] < 0.234)]
-  x2 <- tun[length(tun)-sum(mtun[,5] > 0.234)+1]
-
-  tun2 <- (0.234-y1)*(x2-x1)/(y2-y1)+x1
-
-  y1 <- mtun[sum(mtun[,12] < 0.234),12]
-  y2 <- mtun[length(tun)-sum(mtun[,12] > 0.234)+1,12]
-  x1 <- tun[sum(mtun[,12] < 0.234)]
-  x2 <- tun[length(tun)-sum(mtun[,12] > 0.234)+1]
-
-  tun3 <- (0.234-y1)*(x2-x1)/(y2-y1)+x1 
-  
-  return(c(tun1,tun2,tun3))
-}
-
-
-#RANDOM STARTING POINTS
-pi    <- rgamma(4,1,1)
-pi    <- pi/sum(pi)
-rho   <- rexp(6)
-sigma <- c(1,rexp(3))
 
 #MCMC
 sim   <- 10000000
 lag   <- 1000
-usim <- seq(lag,sim,lag)
 
-smcmc <- matrix(NA,ncol=14,nrow=length(usim))
-t1 <- Sys.time()
-p <- 1
-for (i in 1:sim) {
+mcmc <- function(data,a,b,c,sim,lag,vtun) {
+
+  tun1 <- vtun[1]
+  tun2 <- vtun[1]
+  tun3 <- vtun[1]
   
-  pi    <- mhpi(N,pi,rho,sigma,data,a,tun1)
-  rho   <- mhrho(N,pi,rho,sigma,data,c,tun2)
-  sigma <- mhsigma(N,pi,rho,sigma,data,b,tun3)
+  #RANDOM STARTING POINTS
+  pi    <- rgamma(4,1,1)
+  pi    <- pi/sum(pi)
+  rho   <- rexp(6)
+  sigma <- c(1,rexp(3))
+
+  smcmc <- matrix(NA,ncol=14,nrow=length(usim))
+
+  usim <- seq(lag,sim,lag)
+
+  p <- 1
+  for (i in 1:sim) {
   
-  if (i == usim[p]) {
-    print(paste("% MCMC: ", round(i*100/sim,1)," | pi: ",paste(round(pi,3),collapse=" ")," rho:",paste(round(rho,5),collapse=" ")," sigma:",paste(round(sigma,3),collapse=" "),sep=""))
-    smcmc[p,] <- c(pi,rho,sigma)
-    p<-p+1
+    pi    <- mhpi(N,pi,rho,sigma,data,a,tun1)
+    rho   <- mhrho(N,pi,rho,sigma,data,c,tun2)
+    sigma <- mhsigma(N,pi,rho,sigma,data,b,tun3)
+  
+    if (i == usim[p]) {
+      print(paste("% MCMC: ", round(i*100/sim,1)," | pi: ",paste(round(pi,3),collapse=" ")," rho:",paste(round(rho,5),collapse=" ")," sigma:",paste(round(sigma,3),collapse=" "),sep=""))
+      smcmc[p,] <- c(pi,rho,sigma)
+      p<-p+1
+    }
   }
+
+  return(smcmc)
 }
-t2 <- Sys.time()
+
+smcmc <- mcmc(data,a,b,c,sim,lag,vtun)
+  
 write.table(smcmc,"mcmc.txt",quote=F,row.names=F,col.names=F)
 
-print(t1)
-print(t2)
 
 
 pdf("parameters.pdf", 10, 20)
